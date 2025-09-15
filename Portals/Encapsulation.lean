e13c1e5c7ea757135fa943635e64c77b8e05d80e
crm4244 <collinrmcclellan@gmail.com>
2025-09-15 15:35:18 -0700

    added some basic properties as theorems

--- Portals/Encapsulation.lean
+++ Portals/Encapsulation.lean
@@ -9,61 +9,25 @@ def Encapsulation (E : ℕ → Set X) :=
   (∀ n, IsOpen (E n)) ∧
   (IsCompact (closure (E 0))) ∧
   (∀ n, closure (E (n + 1)) ⊆ E n) ∧
-  (∀ (p q : X), Encapsulation.IsCenter E p → Encapsulation.IsCenter E q → p=q)
+  (∀ p, Encapsulation.IsCenter E p → (∀ q, Encapsulation.IsCenter E q → p=q))
 
+variable {E : ℕ → Set X}
 
 namespace Encapsulation
 
+theorem nth_Nonempty (hE : Encapsulation E) (n : ℕ) : (E n).Nonempty := by
+  rcases hE with ⟨h, _⟩
+  exact h n
 
-theorem center_exists {E : ℕ → Set X} :
-    Encapsulation E → ∃ p, IsCenter E p := by
-  intro hE
-  rcases hE with ⟨hne, ho, hc0, hn, _⟩
-  have h : (⋂ n, closure (E n)).Nonempty := by
-    apply IsCompact.nonempty_iInter_of_sequence_nonempty_isCompact_isClosed
-    · intro n
-      exact subset_trans (hn n) subset_closure
-    · intro n
-      obtain ⟨p, hp⟩ := hne n
-      use p
-      apply subset_closure
-      exact hp
-    · exact hc0
-    · intro n
-      exact isClosed_closure
-  obtain ⟨p, hp⟩ := h
-  use p
-  unfold IsCenter
-  rw [Set.mem_iInter] at hp ⊢
-  intro n
-  apply Set.mem_of_mem_of_subset (hp (n+1))
-  exact hn n
+theorem nth_IsOpen (hE : Encapsulation E) (n : ℕ) : IsOpen (E n) := by
+  rcases hE with ⟨_, h, _⟩
+  exact h n
 
-theorem center_unique {E : ℕ → Set X} :
-    Encapsulation E → ∀ (p q : X), IsCenter E p → IsCenter E q → p=q := by
-  intro hE
-  rcases hE with ⟨_, _, _, _, h⟩
-  exact h
+theorem nth_closure_nested (hE : Encapsulation E) (n : ℕ) : closure (E (n + 1)) ⊆ E n := by
+  rcases hE with ⟨_, _, _, h, _⟩
+  exact h n
 
-noncomputable def center {E : ℕ → Set X} (hE : Encapsulation E) : X :=
-  Classical.choose (center_exists hE)
-
-theorem IsCenter_center {E : ℕ → Set X} (hE : Encapsulation E) :
-    IsCenter E (center hE) := by
-  apply Classical.choose_spec
-
-theorem center_exists_unique (E : ℕ → Set X) :
-    Encapsulation E → ∃! p, IsCenter E p := by
-  intro hE
-  use center hE
-  split_ands
-  · apply IsCenter_center
-  · intro y hy
-    exact center_unique hE y (center hE) hy (IsCenter_center hE)
-
-theorem nested {E : ℕ → Set X} : Encapsulation E → ∀ n m,  n ≤ m → E m ⊆ E n := by
-  intro hE n m h
-  rcases hE with ⟨_, _, _, hn, _⟩
+theorem nested (hE : Encapsulation E) {n m : ℕ} (h : n ≤ m) : E m ⊆ E n := by
   induction m with
   | zero =>
     rw [Nat.le_zero_eq] at h
@@ -74,42 +38,78 @@ theorem nested {E : ℕ → Set X} : Encapsulation E → ∀ n m,  n ≤ m → E
     | inl h =>
       rw [← Nat.le_iff_lt_add_one] at h
       apply subset_trans subset_closure
-      apply subset_trans (hn m)
+      apply subset_trans (nth_closure_nested hE m)
       exact ih h
     | inr h =>
       rw [h]
 
-theorem compact_closures {E : ℕ → Set X} :
-    ∀ n, Encapsulation E → IsCompact (closure (E n)) := by
-  intro n hE
+theorem nth_compact_closure (hE : Encapsulation E) (n : Nat) :
+    IsCompact (closure (E n)) := by
+  have h := nested hE (Nat.zero_le (n-1))
+  rcases hE with ⟨_, _, hc0, hn, _⟩
   cases n with
   | zero =>
-    rcases hE with ⟨_, _, hc0, _⟩
     exact hc0
   | succ n =>
-    have h := nested hE 0 n (Nat.zero_le n)
-    rcases hE with ⟨_, _, hc0, hn, _⟩
     apply IsCompact.of_isClosed_subset hc0 isClosed_closure
     apply subset_trans (hn n)
     apply subset_trans h
     exact subset_closure
 
-theorem i_SubSequence_is_encapsulation {E : ℕ → Set X} {α : ℕ → ℕ} :
-    Encapsulation E → StrictMono α → Encapsulation (E ∘ α) := by
-  intro hE hα
+theorem center_unique (hE : Encapsulation E) {p : X} (hp : IsCenter E p)
+    {q : X} (hq : IsCenter E q) : p=q := by
+  rcases hE with ⟨_, _, _, _, h⟩
+  exact h p hp q hq
+
+theorem center_exists (hE : Encapsulation E) :
+    ∃ p, IsCenter E p := by
+  have h : (⋂ n, closure (E n)).Nonempty := by
+    apply IsCompact.nonempty_iInter_of_sequence_nonempty_isCompact_isClosed
+    · intro n
+      exact subset_trans (nth_closure_nested hE n) subset_closure
+    · intro n
+      obtain ⟨p, hp⟩ := nth_Nonempty hE n
+      use p
+      apply subset_closure
+      exact hp
+    · exact nth_compact_closure hE 0
+    · intro n
+      exact isClosed_closure
+  obtain ⟨p, hp⟩ := h
+  use p
+  unfold IsCenter
+  rw [Set.mem_iInter] at hp ⊢
+  intro n
+  apply Set.mem_of_mem_of_subset (hp (n+1))
+  exact nth_closure_nested hE n
+
+noncomputable def center (hE : Encapsulation E) : X :=
+  Classical.choose (center_exists hE)
+
+theorem IsCenter_center (hE : Encapsulation E) :
+    IsCenter E (center hE) := by
+  apply Classical.choose_spec
+
+theorem center_exists_unique (hE : Encapsulation E) :
+    ∃! p, IsCenter E p := by
+  use center hE
   split_ands
-  · rcases hE with ⟨hne, ho, hc0, hn, hcu⟩
-    intro n
-    exact hne (α n)
-  · rcases hE with ⟨hne, ho, hc0, hn, hcu⟩
-    intro n
-    exact ho (α n)
-  · exact compact_closures (α 0) hE
+  · apply IsCenter_center
+  · intro y hy
+    exact center_unique hE hy (IsCenter_center hE)
+
+theorem i_SubSequence_is_Encapsulation (hE : Encapsulation E) {α : ℕ → ℕ} (hα : StrictMono α) :
+    Encapsulation (E ∘ α) := by
+  split_ands
+  · intro n
+    exact nth_Nonempty hE (α n)
+  · intro n
+    exact nth_IsOpen hE (α n)
+  · exact nth_compact_closure hE (α 0)
   · have h := hE
-    rcases hE with ⟨_, _, _, hn, _⟩
     intro n
     specialize hα (Nat.lt_add_one n)
-    have hn1 := hn (α (n+1) - 1)
+    have hn1 := nth_closure_nested hE (α (n+1) - 1)
     have hα2 := Nat.lt_of_le_of_lt (Nat.zero_le (α n)) hα
     rw [Nat.sub_add_cancel] at hn1
     · apply subset_trans hn1
@@ -117,15 +117,13 @@ theorem i_SubSequence_is_encapsulation {E : ℕ → Set X} {α : ℕ → ℕ} :
       rw [Nat.le_sub_one_iff_lt hα2]
       exact hα
     apply Nat.le_of_pred_lt hα2
-  · have hE2 := hE
-    rcases hE with ⟨hne, ho, hc0, hn, hcu⟩
-    intro p q hp hq
-    apply hcu
+  · intro p hp q hq
+    apply center_unique hE
     all_goals unfold IsCenter at hp hq ⊢
     all_goals rw [Set.mem_iInter] at hp hq ⊢
     all_goals intro n
-    · exact nested hE2 n (α n) (StrictMono.id_le hα n) (hp n)
-    · exact nested hE2 n (α n) (StrictMono.id_le hα n) (hq n)
+    · exact nested hE (StrictMono.id_le hα n) (hp n)
+    · exact nested hE (StrictMono.id_le hα n) (hq n)
 
 
 end Encapsulation
--- Portals/Surface.lean
+++ Portals/Surface.lean
@@ -1,1 +1,33 @@
 import MathLib.Topology.Connected.Basic
+
+
+
+variable {X : Type} [hX : TopologicalSpace X]
+
+def Surface (S : Set X) := IsClosed S ∧ interior S = ∅
+
+variable {S : Set X}
+
+
+
+namespace Surface
+
+
+theorem frontier_eq_self (hS : Surface S) : frontier S = S := by
+  rcases hS with ⟨hS_closed, hS_int_empty⟩
+  unfold frontier
+  rw [hS_int_empty]
+  rw [Set.diff_empty]
+  rw [closure_eq_iff_isClosed]
+  exact hS_closed
+
+theorem IsOpen_compl (hS : Surface S) : IsOpen Sᶜ := by
+  rcases hS with ⟨hS_closed, _⟩
+  exact IsClosed.isOpen_compl
+
+theorem IsOpen_IsOpen_diff (hS : Surface S) {U : Set X} (hU : IsOpen U) :
+    IsOpen (U \ S) := by
+
+  sorry
+
+end Surface
