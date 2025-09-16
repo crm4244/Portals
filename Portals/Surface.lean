daead7b8a0c1c3bf0297c3ef41ec144a401a1410
crm4244 <collinrmcclellan@gmail.com>
2025-09-16 02:07:37 -0700

    simplified some proofs

--- Portals/Surface.lean
+++ Portals/Surface.lean
@@ -14,66 +14,44 @@ variable {S : Set X}
 namespace Surface
 
 
-theorem IsClosed (hS : Surface S) : IsClosed S := by
-  rcases hS with ⟨h, _⟩
-  exact h
+theorem IsClosed (hS : Surface S) : IsClosed S := hS.1
 
-theorem interior_eq_empty (hS : Surface S) : interior S = ∅ := by
-  rcases hS with ⟨_, h⟩
-  exact h
+theorem interior_eq_empty (hS : Surface S) : interior S = ∅ := hS.2
 
 theorem frontier_eq_self (hS : Surface S) : frontier S = S := by
-  rcases hS with ⟨hS_closed, hS_int_empty⟩
-  unfold frontier
-  rw [hS_int_empty]
-  rw [Set.diff_empty]
-  rw [closure_eq_iff_isClosed]
-  exact hS_closed
+  rw [frontier, hS.2, Set.diff_empty]
+  exact closure_eq_iff_isClosed.mpr hS.1
 
 theorem closure_eq_self (hS : Surface S) : closure S = S :=
   IsClosed.closure_eq (IsClosed hS)
 
 
-theorem IsOpen_compl (hS : Surface S) : IsOpen Sᶜ := by
-  rcases hS with ⟨hS_closed, _⟩
-  exact IsClosed.isOpen_compl
+theorem IsOpen_compl (hS : Surface S) : IsOpen Sᶜ := isOpen_compl_iff.mpr hS.1
 
-theorem IsOpen_diff (hS : Surface S) {U : Set X} (hU : IsOpen U) :
-    IsOpen (U \ S) :=
+theorem IsOpen_diff (hS : Surface S) {U : Set X} (hU : IsOpen U) : IsOpen (U \ S) :=
   IsOpen.sdiff hU (IsClosed hS)
 
 
 
-theorem hh (hS : Surface S) {U : Set X} (hU : IsOpen U) :
+theorem inter_closure_subset_frontier_diff (hS : Surface S) {U : Set X} (hU : IsOpen U) :
     S ∩ closure U ⊆ frontier (U \ S) := by
   intro p ⟨hpS, hpU⟩
   unfold frontier
   rw [Set.mem_diff]
   split_ands
-  · rw [mem_closure_iff] at hpU ⊢
+  · apply mem_closure_iff.mpr
     intro V hV hpV
-    specialize hpU V hV hpV
     rw [← compl_compl S, Set.diff_compl, ← Set.inter_assoc]
     rw [← Set.diff_compl, compl_compl]
     apply Set.nonempty_of_not_subset
     intro hVU
-    have hVUi : V ∩ U ⊆ interior S := by
-      apply (IsOpen.subset_interior_iff (IsOpen.inter hV hU)).mpr
-      exact hVU
-    rw [interior_eq_empty hS, Set.subset_empty_iff] at hVUi
-    rw [hVUi] at hpU
-    exact Set.not_nonempty_empty hpU
+    have hVUi := (IsOpen.subset_interior_iff (IsOpen.inter hV hU)).mpr hVU
+    rw [interior_eq_empty hS] at hVUi
+    exact Set.nonempty_iff_ne_empty.mp
+      (mem_closure_iff.mp hpU V hV hpV)
+      (Set.subset_empty_iff.mp hVUi)
+  · exact fun h => ((Set.mem_diff p).mp (interior_subset h)).2 hpS
 
-
-
-  · intro hpUI
-    apply interior_subset at hpUI
-    rw [Set.mem_diff] at hpUI
-    cases hpUI
-    contradiction
 
 
 end Surface
-
-
-#check mem_closure_iff
