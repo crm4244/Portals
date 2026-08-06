3d94f0d1ed78ad0c78019cb00b8c5b831411ed32
crm4244 <collinrmcclellan@gmail.com>
2026-08-05 23:03:57 -0700

    made transport_symmetry and \McG_trans instance variables

--- Portals/CategoryTheory/MaterialSpace.lean
+++ Portals/CategoryTheory/MaterialSpace.lean
@@ -62,104 +62,116 @@ theorem continuous_transportOf (P : Equiv.Perm F) (f : F) :
     P f |>.1.2.continuous.comp <| continuous_subtype_val.comp f.1.homeomorph.continuous_symm
 
 
-variable (transport_symmetry : ∀ P (f g : F) (q : X) (hf : q ∈ f.1.range) (hg : q ∈ g.1.range),
-  transportOf P ⟨q, hf⟩ = transportOf P ⟨q, hg⟩)
 
 
-noncomputable def pretransport (P : Equiv.Perm F) (x : 𝒰 F) : X :=
-  iUnionLift _ _ (transport_symmetry P) _ subset_rfl x
+class TransportSymmetry (symmetricPerms : Subgroup (Equiv.Perm F)) : Prop where
+  symmetry : ∀ (P : symmetricPerms) (f g : F) (q : X) (hf : q ∈ f.1.range) (hg : q ∈ g.1.range),
+    transportOf P.1 ⟨q, hf⟩ = transportOf P.1 ⟨q, hg⟩
 
 
-theorem pretransport_mem (P : Equiv.Perm F) (x : 𝒰 F) :
-  pretransport transport_symmetry P x ∈ 𝒰 F :=
+variable {symmetricPerms : Subgroup (Equiv.Perm F)}
+variable [TransportSymmetry symmetricPerms]
+
+
+
+noncomputable def pretransport (P : symmetricPerms) (x : 𝒰 F) : X :=
+  iUnionLift _ _ (‹TransportSymmetry symmetricPerms›.symmetry P) _ subset_rfl x
+
+
+theorem pretransport_mem (P : symmetricPerms) (x : 𝒰 F) :
+  pretransport P x ∈ 𝒰 F :=
     mem_iUnion_of_mem _ <| mem_range_self _
 
 
-theorem pretransport_eq_transportOf (P : Equiv.Perm F) {p : 𝒰 F} {f : F} (hp : p.1 ∈ f.1.range) :
-  pretransport transport_symmetry P p = transportOf P ⟨p, hp⟩ :=
+theorem pretransport_eq_transportOf (P : symmetricPerms) {p : 𝒰 F} {f : F} (hp : p.1 ∈ f.1.range) :
+  pretransport P p = transportOf P.1 ⟨p, hp⟩ :=
     iUnionLift_of_mem _ _
 
 
-noncomputable def transport (P : Equiv.Perm F) (x : 𝒰 F) : 𝒰 F :=
-  ⟨pretransport transport_symmetry P x, pretransport_mem transport_symmetry P x⟩
+noncomputable def transport (P : symmetricPerms) (x : 𝒰 F) : 𝒰 F :=
+  ⟨pretransport P x, pretransport_mem P x⟩
 
 
-theorem transport_eq_transportOf (P : Equiv.Perm F) {p : 𝒰 F} {f : F} (hp : p.1 ∈ f.1.range) :
-  transport transport_symmetry P p = ⟨_, transportOf_mem_𝒰 P ⟨p, hp⟩⟩ :=
+theorem transport_eq_transportOf (P : symmetricPerms) {p : 𝒰 F} {f : F} (hp : p.1 ∈ f.1.range) :
+  transport P p = ⟨_, transportOf_mem_𝒰 P.1 ⟨p, hp⟩⟩ :=
     Subtype.mk_eq_mk.mpr <| iUnionLift_of_mem _ _
 
 
-theorem val_transport_mem_range (P : Equiv.Perm F) {p : 𝒰 F} {f : F} (hp : p.1 ∈ f.1.range) :
-  ↑(transport transport_symmetry P p) ∈ (P f).1.range :=
+theorem val_transport_mem_range (P : symmetricPerms) {p : 𝒰 F} {f : F} (hp : p.1 ∈ f.1.range) :
+  ↑(transport P p) ∈ (P.1 f).1.range :=
     let ⟨y, h⟩ := hp; ⟨y, by
-    rw [transport_eq_transportOf transport_symmetry P ⟨y, h⟩]
-    exact congr_arg (P f).1 <| f.1.2.injective <| h.trans <| f.1.inv_right ⟨p.1, y, h⟩ |>.symm⟩
+    rw [transport_eq_transportOf P ⟨y, h⟩]
+    exact congr_arg (P.1 f).1 <| f.1.2.injective <| h.trans <| f.1.inv_right ⟨p.1, y, h⟩ |>.symm⟩
 
 
-theorem transport_mul_apply (P Q : Equiv.Perm F) (x : 𝒰 F) :
-  transport transport_symmetry (P * Q) x =
-    transport transport_symmetry P (transport transport_symmetry Q x) := by
+-- this needs closure of symmetricPerms under *
+theorem transport_mul_apply (P Q : symmetricPerms) (x : 𝒰 F) :
+  transport (P * Q) x =
+    transport P (transport Q x) := by
 
   let f : F := Classical.choose <| mem_iUnion.mp x.2
-  let g := Q f
+  let g := Q.1 f
   have hx : x.1 ∈ f.1.range := Classical.choose_spec <| mem_iUnion.mp x.2
 
-  let y := transport transport_symmetry Q x
+  let y := transport Q x
   have hy : y.1 ∈ g.1.range := exists_apply_eq_apply g.1 _
 
-  rw [transport_eq_transportOf transport_symmetry P hy]
-  rw [transport_eq_transportOf transport_symmetry (P * Q) hx]
+  rw [transport_eq_transportOf P hy]
+  rw [transport_eq_transportOf (P * Q) hx]
 
-  apply Subtype.mk_eq_mk.mpr <| congr_arg (P g).1 <| g.1.2.injective _
+  apply Subtype.mk_eq_mk.mpr <| congr_arg (P.1 g).1 <| g.1.2.injective _
   rw [g.1.inv_right]
   unfold y
   simp only
-  rw [transport_eq_transportOf transport_symmetry Q hx]
+  rw [transport_eq_transportOf Q hx]
   rfl
 
 
-theorem transport_mul (P Q : Equiv.Perm F) :
-  transport transport_symmetry (P * Q) =
-    transport transport_symmetry P ∘ transport transport_symmetry Q :=
-  funext <| transport_mul_apply transport_symmetry P Q
+theorem transport_mul (P Q : symmetricPerms) :
+  transport (P * Q) =
+    transport P ∘ transport Q :=
+  funext <| transport_mul_apply P Q
 
 
-theorem transport_one_apply (x : 𝒰 F) : transport transport_symmetry 1 x = x := by
-  rw [transport_eq_transportOf transport_symmetry 1 <| Classical.choose_spec <| mem_iUnion.mp x.2]
+theorem transport_one_apply (x : 𝒰 F) : transport (symmetricPerms := symmetricPerms) 1 x = x := by
+  rw [transport_eq_transportOf 1 <| Classical.choose_spec <| mem_iUnion.mp x.2]
   exact Subtype.mk_eq_mk.mpr <| PortalMap.inv_right _ _
 
 
-theorem transport_one : transport transport_symmetry 1 = id :=
-  funext fun x ↦ transport_one_apply transport_symmetry x
+theorem transport_one : transport (symmetricPerms := symmetricPerms) 1 = id :=
+  funext fun x ↦ transport_one_apply x
 
 
-theorem continuous_transport (P : Equiv.Perm F) : Continuous (transport transport_symmetry P) :=
-  continuous_of_continuousOn_iUnion_of_isOpen
-    (fun f ↦ continuousOn_iff_continuous_restrict.mpr <| continuous_transportOf P f
-      |>.subtype_mk_self_mk_val_val _ _ |>.congr
-        fun x ↦ transport_eq_transportOf transport_symmetry P
-          (mem_range_of_range_inclusion x) |>.symm)
-    (fun f ↦ f.1.2.isOpen_range.isOpenMap_inclusion (range_subset_𝒰 f) |>.isOpen_range)
-    union_range_inclusion_eq_univ
+theorem continuous_transport (P : symmetricPerms) :
+  Continuous (transport P) :=
+    continuous_of_continuousOn_iUnion_of_isOpen
+      (fun f ↦ continuousOn_iff_continuous_restrict.mpr <| continuous_transportOf P f
+        |>.subtype_mk_self_mk_val_val _ _ |>.congr
+          fun x ↦ transport_eq_transportOf P
+            (mem_range_of_range_inclusion x) |>.symm)
+      (fun f ↦ f.1.2.isOpen_range.isOpenMap_inclusion (range_subset_𝒰 f) |>.isOpen_range)
+      union_range_inclusion_eq_univ
 
 
-theorem transport_inv_left (P : Equiv.Perm F) (x : ↑(𝒰 F)) :
-  transport transport_symmetry (Equiv.symm P) (transport transport_symmetry P x) = x :=
-    (transport_mul_apply _ _ _ x).symm ▸ P.symm_mul ▸ transport_one_apply transport_symmetry x
+
+theorem transport_inv_left (P : symmetricPerms) (x : ↑(𝒰 F)) :
+  transport P⁻¹ (transport P x) = x :=
+    transport_mul_apply _ _ _ |>.symm.trans <|
+      inv_mul_cancel P ▸ transport_one_apply (symmetricPerms := symmetricPerms) x
 
 
-theorem transport_inv_right (P : Equiv.Perm F) (x : ↑(𝒰 F)) :
-  transport transport_symmetry P (transport transport_symmetry (Equiv.symm P) x) = x :=
-    transport_inv_left transport_symmetry P.symm x
+theorem transport_inv_right (P : symmetricPerms) (x : ↑(𝒰 F)) :
+  transport P (transport P⁻¹ x) = x :=
+    transport_inv_left P⁻¹ x
 
 
-noncomputable instance instHomeomorphTransport (P : Equiv.Perm F) : Homeomorph (𝒰 F) (𝒰 F) where
-  toFun := transport transport_symmetry P
-  invFun := transport transport_symmetry P.symm
-  left_inv := transport_inv_left transport_symmetry P
-  right_inv := transport_inv_right transport_symmetry P
-  continuous_toFun := continuous_transport transport_symmetry P
-  continuous_invFun := continuous_transport transport_symmetry P.symm
+noncomputable instance instHomeomorphTransport (P : symmetricPerms) : 𝒰 F ≃ₜ 𝒰 F where
+  toFun := transport P
+  invFun := transport P⁻¹
+  left_inv := transport_inv_left P
+  right_inv := transport_inv_right P
+  continuous_toFun := continuous_transport P
+  continuous_invFun := continuous_transport P⁻¹
 
 /-
 theorem 𝒮_subset_𝒰 (F : Set (PortalMap Y X)) (S : Set Y) : 𝒮 F S ⊆ 𝒰 F :=
@@ -170,13 +182,13 @@ theorem 𝒮_subset_𝒰 (F : Set (PortalMap Y X)) (S : Set Y) : 𝒮 F S ⊆ �
 abbrev 𝒮' (F : Set (PortalMap X Y)) (S : Set Y) := Sides.restrict_surface (𝒮 F S) (𝒰 F)
 
 
-theorem transport_mem_𝒮'_of_mem {S : Set Y} (P : Equiv.Perm F) {x : 𝒰 F} :
+theorem transport_mem_𝒮'_of_mem {S : Set Y} (P : symmetricPerms) {x : 𝒰 F} :
   x ∈ 𝒮' F S →
-    transport transport_symmetry P x ∈ 𝒮' F S :=
+    transport P x ∈ 𝒮' F S :=
   fun ⟨_, ⟨f, rfl⟩, y, hy, hf⟩ ↦
-    mem_preimage.mpr <| mem_iUnion.mpr ⟨P f, ⟨y, hy, by
-      rw [transport_eq_transportOf transport_symmetry P ⟨y, hf⟩]
-      exact congr_arg (P f).1 <| f.1.2.injective <| hf.trans <| f.1.inv_right ⟨x, y, hf⟩ |>.symm⟩⟩
+    mem_preimage.mpr <| mem_iUnion.mpr ⟨P.1 f, ⟨y, hy, by
+      rw [transport_eq_transportOf P ⟨y, hf⟩]
+      exact congr_arg (P.1 f).1 <| f.1.2.injective <| hf.trans <| f.1.inv_right ⟨x, y, hf⟩ |>.symm⟩⟩
 
 /-
 theorem image_transport_𝒮'_eq (S : Set Y) (P : Equiv.Perm F) :
@@ -191,38 +203,38 @@ variable {S : Set Y}
 
 
 
-noncomputable def Sides.transport (P : Equiv.Perm F) : Sides (𝒮' F S) → Sides (𝒮' F S) :=
-  map (f := Portal.transport _ _) (instHomeomorphTransport transport_symmetry P).isEmbedding
+noncomputable def Sides.transport (P : symmetricPerms) : Sides (𝒮' F S) → Sides (𝒮' F S) :=
+  map (f := Portal.transport _ _) (instHomeomorphTransport P).isEmbedding
 
 
 
-theorem Sides.transport_center_comm (P : Equiv.Perm F) (σ : Sides (𝒮' F S)) :
-  (σ.transport transport_symmetry P).center =
-    Portal.transport transport_symmetry P σ.center := map_comm _ _
+theorem Sides.transport_center_comm (P : symmetricPerms) (σ : Sides (𝒮' F S)) :
+  (σ.transport P).center =
+    Portal.transport P σ.center := map_comm _ _
 
 
-noncomputable def Sides.transport_at (P : Equiv.Perm F) {p : 𝒰 F} (σ : at_point (𝒮' F S) p) :
-  at_point (𝒮' F S) (Portal.transport transport_symmetry P p) :=
-    ⟨σ.1.transport transport_symmetry P, mem_setOf.mpr <|
-      transport_center_comm transport_symmetry P σ.1 |>.trans <| σ.2.symm ▸ rfl⟩
+noncomputable def Sides.transport_at (P : symmetricPerms) {p : 𝒰 F} (σ : at_point (𝒮' F S) p) :
+  at_point (𝒮' F S) (Portal.transport P p) :=
+    ⟨σ.1.transport P, mem_setOf.mpr <|
+      transport_center_comm P σ.1 |>.trans <| σ.2.symm ▸ rfl⟩
 
 
-theorem transport_relevant (P : Equiv.Perm F) {p : 𝒰 F} (f : relevant_portal_maps F p) :
-  pretransport transport_symmetry P p ∈ (P f.1 |>.1.range) :=
+theorem transport_relevant (P : symmetricPerms) {p : 𝒰 F} (f : relevant_portal_maps F p) :
+  pretransport P p ∈ (P.1 f.1 |>.1.range) :=
     let ⟨y, hy⟩ := f.2; ⟨y,
-      (congr_arg (P f.1).1 <| f.1.1.2.injective <| hy.trans <|
+      (congr_arg (P.1 f.1).1 <| f.1.1.2.injective <| hy.trans <|
         f.1.1.inv_right ⟨p, f.2⟩ |>.symm).trans <|
-      pretransport_eq_transportOf transport_symmetry P f.2 |>.symm⟩
+      pretransport_eq_transportOf P f.2 |>.symm⟩
 
 
-theorem rusto_transport_eq (P : Equiv.Perm F)
+theorem rusto_transport_eq (P : symmetricPerms)
   {a : Sides (𝒮 F S)} {f : F} (hf : a.center ∈ f.1.range) :
     restricted_union_side_to_original (a.restrict_of_mem (U := f.1.opens_range) hf) =
     restricted_union_side_to_original (a.restrict_of_mem (range_subset_𝒰 f hf)
-      |>.transport transport_symmetry P |>.lift.restrict_of_mem (U := (P f).1.opens_range)
+      |>.transport P |>.lift.restrict_of_mem (U := (P.1 f).1.opens_range)
       ((Sides.lift_comm _ |>.trans <| congr_arg Subtype.val <|
-        Sides.transport_center_comm transport_symmetry P _) ▸
-        (val_transport_mem_range _ P <| a.center_restrict_comm (range_subset_𝒰 f hf) ▸ hf))) :=
+        Sides.transport_center_comm P _) ▸
+        (val_transport_mem_range P <| a.center_restrict_comm (range_subset_𝒰 f hf) ▸ hf))) :=
   by sorry
 
 
@@ -242,25 +254,30 @@ noncomputable def quattle (γ : GluingPattern S (Equiv.Perm F))
 
 
 
-variable {γ : GluingPattern S (Equiv.Perm F)}
-variable {Γ : GeneralizedMultiset (Equiv.Perm F) → Equiv.Perm F}
-variable (𝒢_trans : ∀ {p : X} (a b c : Sides.at_point (𝒮 F S) p),
-    Γ (quattle γ a b) * Γ (quattle γ b c) = Γ (quattle γ a c))
-variable (transport_symmetry : ∀ P (f g : F) (q : X) (hf : q ∈ f.1.range) (hg : q ∈ g.1.range),
-    transportOf P ⟨q, hf⟩ = transportOf P ⟨q, hg⟩)
+variable (γ : GluingPattern S (Equiv.Perm F))
+variable (Γ : GeneralizedMultiset (Equiv.Perm F) → Equiv.Perm F)
+variable (symmetricPerms : Subgroup (Equiv.Perm F))
+
+
+class CombineTrans : Prop where
+  trans : ∀ {p : X} (a b c : Sides.at_point (𝒮 F S) p),
+    Γ (quattle γ a b) * Γ (quattle γ b c) = Γ (quattle γ a c)
+
+
+variable [CombineTrans γ Γ] [TransportSymmetry symmetricPerms]
 
 noncomputable def combinedGluingPattern : GluingPattern (𝒮 F S) (Equiv.Perm F) :=
-  { map a b := Γ (quattle γ a b), trans := 𝒢_trans}
+  { map a b := Γ (quattle γ a b), trans := ‹CombineTrans γ Γ›.trans}
 
-noncomputable abbrev 𝒢 := combinedGluingPattern 𝒢_trans
+noncomputable abbrev 𝒢 := combinedGluingPattern γ Γ
 
 
 open GenMulti in theorem simultaneous_transport
-  (P : Equiv.Perm F) {p : 𝒰 F} (a b : Sides.at_point (𝒮' F S) p) :
-    𝒢 𝒢_trans
-      (Sides.lift_at <| Sides.transport_at transport_symmetry P a)
-      (Sides.lift_at <| Sides.transport_at transport_symmetry P b) =
-    𝒢 𝒢_trans (Sides.lift_at a) (Sides.lift_at b) := by
+  (P : symmetricPerms) {p : 𝒰 F} (a b : Sides.at_point (𝒮' F S) p) :
+    𝒢 γ Γ
+      (Sides.lift_at <| Sides.transport_at P a)
+      (Sides.lift_at <| Sides.transport_at P b) =
+    𝒢 γ Γ (Sides.lift_at a) (Sides.lift_at b) := by
 
   apply congr_arg Γ <| Quotient.eq.mpr _
   symm
@@ -268,13 +285,13 @@ open GenMulti in theorem simultaneous_transport
   simp only
   use {
     -- maybe build this equiv in the transport section
-    toFun f := ⟨P f.1, transport_relevant transport_symmetry P f⟩
-    invFun f := ⟨P.symm f.1, by
+    toFun f := ⟨P.1 f.1, transport_relevant P f⟩
+    invFun f := ⟨P⁻¹.1 f.1, by
       #check f.2
-      #check transport_relevant transport_symmetry P.symm ⟨_, sorry⟩
+      #check transport_relevant P⁻¹ ⟨_, sorry⟩
       sorry⟩
-    left_inv f := Subtype.eq <| P.symm_apply_apply f.1
-    right_inv f := Subtype.eq <| P.apply_symm_apply f.1
+    left_inv f := Subtype.eq <| P.1.symm_apply_apply f.1
+    right_inv f := Subtype.eq <| P.1.apply_symm_apply f.1
   }
 
   unfold Function.comp
@@ -291,17 +308,17 @@ open GenMulti in theorem simultaneous_transport
 def matspace_rel (a b : Sides (𝒮 F S)) : Prop :=
   a = b ∨ ∃ (ha : a.center ∈ 𝒰 F) (a' : Sides (𝒮 F S)) (ha' : a'.center = a.center),
     (a'.restrict_of_mem (ha' ▸ ha) |>.transport
-      transport_symmetry (𝒢 𝒢_trans ⟨a, rfl⟩ ⟨a', ha'⟩)).lift = b
+      ⟨𝒢 γ Γ ⟨a, rfl⟩ ⟨a', ha'⟩, (sorry : _ ∈ symmetricPerms)⟩).lift = b
 
 
-instance instEquivalenceMatSpaceRel : Equivalence <| matspace_rel 𝒢_trans transport_symmetry where
+instance instEquivalenceMatSpaceRel : Equivalence <| matspace_rel γ Γ symmetricPerms where
   refl a := Or.inl rfl
   symm {a b} hab := by
     apply Or.elim hab (Or.inl ·.symm)
     intro ⟨ha, a', ha', hb⟩
     apply Or.inr
     use hb.symm ▸ Sides.center_mem_of_restricted _
-    use a.restrict_of_mem.transport transport_symmetry (𝒢 𝒢_trans ⟨a, rfl⟩ ⟨a', ha'⟩) |>.lift
+    use a.restrict_of_mem.transport ⟨𝒢 γ Γ ⟨a, rfl⟩ ⟨a', ha'⟩, (sorry : _ ∈ symmetricPerms)⟩ |>.lift
 
     use by
       rw [hb.symm]
@@ -337,17 +354,17 @@ instance instEquivalenceMatSpaceRel : Equivalence <| matspace_rel 𝒢_trans tra
 
 
 instance instSetoidSides𝒮 : Setoid (Sides (𝒮 F S)) where
-  r := matspace_rel 𝒢_trans transport_symmetry
-  iseqv := instEquivalenceMatSpaceRel 𝒢_trans transport_symmetry
+  r := matspace_rel γ Γ symmetricPerms
+  iseqv := instEquivalenceMatSpaceRel γ Γ symmetricPerms
 
 
-def MatSpace := Quotient (instSetoidSides𝒮 𝒢_trans transport_symmetry)
+def MatSpace := Quotient (instSetoidSides𝒮 γ Γ symmetricPerms)
 
 
 namespace MatSpace
 
 
-instance : TopologicalSpace (MatSpace 𝒢_trans transport_symmetry) := instTopologicalSpaceQuotient
+instance : TopologicalSpace (MatSpace γ Γ symmetricPerms) := instTopologicalSpaceQuotient
 
 -- woohoo!!!
 
